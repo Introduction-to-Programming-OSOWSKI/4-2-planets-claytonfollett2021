d44d2e818695ead3806c7d4caa974d31527e631d
claytonfollett2021 <813551@mystma.org>
2021-03-02 16:47:28 +0000

    planets3

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def planets(p):
 
-    for i in range(0,len(planet)):
-        if p ==planet[i]:
+    for i in range(0, len(planet)):
+        if p == planet[i]:
             return i
 
     return p + " is not a planet"
